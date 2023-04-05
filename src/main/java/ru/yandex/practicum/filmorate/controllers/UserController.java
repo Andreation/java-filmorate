32b5cb50804093d7f0160c5a1bc86a2ac93d6fd7
Andreation <115004597+Andreation@users.noreply.github.com>
2023-04-06 01:26:19 +1000

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 @RequestMapping("users")
 public class UserController {
     protected UserStorage userStorage = new UserStorage();
+
     @PostMapping()
     public User post(@RequestBody @Valid User user) throws IdException {
         userStorage.save(user);
