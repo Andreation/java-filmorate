ea3de4a66984c41506ba615c5cb89ccc4334aaad
Andreation <115004597+Andreation@users.noreply.github.com>
2023-04-05 22:45:07 +1000

    Delete src directory

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmDateException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class FilmDateException extends Exception {
-    public FilmDateException(String message){
-        super(message);
-    }
-}
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IdException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class IdException extends Exception {
-    public IdException(String message){
-        super(message);
-    }
-}
