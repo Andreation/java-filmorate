69364955c378274985f7bfe16ac04f4016973304
Andreation <115004597+Andreation@users.noreply.github.com>
2023-04-05 22:40:01 +1000

    init commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/storages/FilmStorage.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.storages;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.filmorate.exceptions.IdException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.*;
+@Slf4j
+public class FilmStorage {
+    Map<Integer, Film> films = new LinkedHashMap<>();
+    protected Integer id = 0;
+    public void save(Film film) throws IdException {
+        if (!films.containsKey(film.getId())) {
+            film.setId(++id);
+            films.put(film.getId(), film);
+        } else {
+            throw new IdException("Не верно введен id");
+        }
+    }
+    public void update(Film film) throws IdException{
+        if (films.containsKey(film.getId())){
+            films.put(film.getId(), film);
+        } else {
+            throw new IdException("Не верно введен id");
+        }
+    }
+    public ArrayList<Film> getFilms() {
+        System.out.println(films.toString());
+        return new ArrayList<>(films.values());
+    }
+}
