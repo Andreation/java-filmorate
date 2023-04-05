c7a2504e060e27bb5bd1783c713c9af9ebd0bb06
Andreation <115004597+Andreation@users.noreply.github.com>
2023-04-05 22:40:01 +1000

    init commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmDateException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class FilmDateException extends Exception {
+    public FilmDateException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/validate/ValidateService.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.validate;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exceptions.FilmDateException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.ValidationException;
+import java.time.LocalDate;
+@Component
+public class ValidateService {
+    public void validateFilmDate(Film film) throws FilmDateException {
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895,2, 27))){
+            throw new FilmDateException("Ошибка даты");
+        }
+    }
+}
