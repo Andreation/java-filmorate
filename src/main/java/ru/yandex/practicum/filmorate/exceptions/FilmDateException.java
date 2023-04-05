acbae04e346d4180b53df1c035a1cd990a39703f
Andreation <115004597+Andreation@users.noreply.github.com>
2023-04-06 01:35:06 +1000

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmDateException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmDateException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class FilmDateException extends Exception {
+public class FilmDateException extends Exception{
     public FilmDateException(String message){
         super(message);
     }
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IdException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/IdException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class IdException extends Exception {
+public class IdException extends Exception{
     public IdException(String message){
         super(message);
     }
